0978de8fb1c248750fcd10ea0d9c84fffb8d1422
mseyitoglu <mseyitoglu@outlook.com>
2023-10-11 07:28:22 -0500

    post method

--- flask-03-04-If-Handling-Routes-and-Get-Post-Methods/flask-04-handling-forms-POST-GET-Methods/flask-04-handling-forms/app.py
+++ flask-03-04-If-Handling-Routes-and-Get-Post-Methods/flask-04-handling-forms-POST-GET-Methods/flask-04-handling-forms/app.py
@@ -1,7 +1,8 @@
 # Import Flask modules
+from flask import Flask, render_template, redirect, request, url_for
 
 # Create an object named app
-
+app =Flask(__name__)
 
 # Create welcome page with main.html file and assing it to the root path
 
@@ -15,5 +16,23 @@
 # and template files named `login.html` and `secure.html` given under `templates` folder 
 # and assign to the static route of ('login')
 
+# Write a function named `login` which uses `GET` and `POST` methods, 
+# and template files named `login.html` and `secure.html` given under `templates` folder 
+# and assign to the static route of ('login')
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        user_name = request.form.get("username")
+        pw = request.form.get("password")
+        if user_name == "clarusway" and pw == "clarusway":
+            return render_template("secure.html", user=user_name)
+        else:
+            return render_template("login.html", user=user_name, control = True)
+    else:
+        return render_template("login.html", control=False)
 
+
+# Add a statement to run the Flask application which can be reached from any host.
+if __name__ == '__main__':
+    app.run("0.0.0.0")
 # Add a statement to run the Flask application which can be reached from any host.
