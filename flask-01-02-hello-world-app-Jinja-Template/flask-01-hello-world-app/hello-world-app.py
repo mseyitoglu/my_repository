0998e07459a8bba511b052fd46441abb657ebace
mseyitoglu <mseyitoglu@outlook.com>
2023-10-31 13:06:06 -0500

    Flask01

--- flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
+++ flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
@@ -1,22 +1,29 @@
 # Hello World App
 from flask import Flask
-app = Flask(__name__)
-@app.route("/")
+app=Flask(__name__)
+@app.route("/") #That means: anytime someone visits my webpage, just return the 2 lines to them:
 def hello():
-    return "<h1> Hello World </h1>"
-#Create a function second which returns a string This is the second page and 
-# assign a URL route the second function with decorator @app.route('/second').
+    return "Hello World"
+
+#Create a function that will return the second page and assign a URL 
+# route to the second function with decorator @app.route("/") 
 @app.route("/second")
 def second():
-    return "<h2> This is the second page</h2>"
+    return "<h2>This is the second page</h2>"
+@app.route("/third/subthird")
+def third():
+    return "<h3>This is the subpage of third page</h3>"
 
+#create a dynamic url that takes id number dynamically and return with a 
+# message which shows id of the page
 @app.route("/fourth/<string:id>")
 def fourth(id):
+    
     if id.isdigit():
-        return f"the id of this page is {id}"
+        return f"The id of the page is {id}"
     else:
-        return "Not a valid id"
-
-
-#run the flask app
+        return f"not a valid id"
+        
+#Run the flask app
 app.run(debug=False)
+  
